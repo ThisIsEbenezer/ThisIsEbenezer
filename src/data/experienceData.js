6d30ade05d06fb1a9507b19df55ff5ed12d8488d
Samuel Ebenezer <samuelebenezer1998@gmail.com>
2023-09-13 21:24:27 +0530

    Mofied Jon ttitle

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -2,13 +2,13 @@ export const experienceData = [
     {
         id: 1,
         company: 'HTC Global Services Pvt Ltd',
-        jobtitle: 'Programming Analyst',
+        jobtitle: 'Programmer Analyst',
         startYear: 'Dec 2020',
         endYear: 'Present'
     },
     {
         id: 2,
-        company: 'Jesus Calls - NGO',
+        company: 'J C',
         jobtitle: 'Web Developer',
         startYear: 'May 2019',
         endYear: 'Dec 2020'
